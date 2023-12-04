4c15fd59ffe767aa80fc307fad17f0a8a1a47f1c
Berk Canpolat <canpolatberk19@gmail.com>
2023-12-04 20:45:18 +0300

    Arrays added

---
+++ MyPlayground.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var myFavoriteMovies = ["Pulp","Kill Bill","Reservoir Dogs"]
+
+myFavoriteMovies[0]
+myFavoriteMovies[1].uppercased()
+myFavoriteMovies.count
+myFavoriteMovies[myFavoriteMovies.count - 1]
+myFavoriteMovies.first
+myFavoriteMovies.last
+myFavoriteMovies.sort()
+print(myFavoriteMovies)
+
+
+var myNumber = [1,2,3,4,5,6,7]
+
+myNumber.append(8)
+print(myNumber)
+myNumber[0] = 21
+myNumber[0]
+print(myNumber)
