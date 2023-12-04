d0d329b3ae533ad2669bd6d33300eccbb8bef308
Berk Canpolat <canpolatberk19@gmail.com>
2023-12-04 14:46:22 +0300

    String,Variables,Int,Double,Bool added

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,65 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+//Variables(değişken)
+var myNumber = 5 * 5
+print(myNumber)
+
+10 * 5
+
+//Bu yazım türüne "camilCase" denir. Ve genelde bu kullanılır.
+
+var userName = "Berk"
+print(userName)
+
+//Mevcut Text'imizin sonuna ekler
+userName.append("Ekledim")
+
+//Mevcut Text'imizin bütün harflerini küçültür
+userName.lowercased()
+
+//Mevcut Text'imizin bütün harflerini büyültür
+userName.uppercased()
+print(userName)
+
+
+//Bu yazım türüne "Snack Case" denir.
+//var user_sur_name = "Canpolat"
+
+
+
+//Constants(sabit)
+//Integer & Double & Float
+
+//Integer
+
+let userAge = 24
+
+let myVariable = 4
+
+userAge / myVariable
+
+
+//Double
+
+let userAgeD = 50.0
+let myNumberD = 4.0
+
+userAgeD / myNumberD
+
+//Bool
+var myBoolean = false
+myBoolean = true
+
+
+
+let myString: String = "50"
+let anotherNumber: Int = 30
+
+//define
+var myVerib: String
+
+myVerib = "Test"
+myVerib.uppercased()
