dde1312c29de573edf0ceb4c308adeddfc943e4f
Berk Canpolat <canpolatberk19@gmail.com>
2023-12-11 11:47:39 +0300

    IOS Bootcamp Started and 1.Gun added

---
+++ IOS-BootCamp/1-Gun/StandartProgramlama.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+///Karşılaştırma operatörleri
+var a = 40
+var b = 50
+
+var x = 90
+var y = 80
+
+print("a == b : \(a == b)")
+print("a != b : \(a != b)")
+print("a > b : \(a > b)")
+print("a >= b : \(a >= b)")
+print("a < b : \(a < b)")
+print("a <= b : \(a <= b)")
+
+
