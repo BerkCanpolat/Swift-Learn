f5af7ca8fec10d312d865c1c2227d7120cb7a9cf
Berk Canpolat <canpolatberk19@gmail.com>
2023-12-15 00:18:42 +0300

    tabbar ozellestirme

--- IOS-BootCamp/5.gun/TabbarKullanimi/TabbarKullanimi/ViewController.swift
+++ IOS-BootCamp/5.gun/TabbarKullanimi/TabbarKullanimi/ViewController.swift
@@ -11,7 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let tabItems = tabBarController?.tabBar.items {
+            let anasayfaItem = tabItems[0]
+            let ayarlarItem = tabItems[1]
+            
+            anasayfaItem.badgeValue = "3"
+            ayarlarItem.badgeValue = "Yeni"
+        }
+        
+        let app = UITabBarAppearance()
+        
+        app.backgroundColor = UIColor.systemIndigo
+        
+        renkDegis(item: app.stackedLayoutAppearance)
+        renkDegis(item: app.compactInlineLayoutAppearance)
+        renkDegis(item: app.inlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = app
+        tabBarController?.tabBar.scrollEdgeAppearance = app
+        
+    }
+    
+    func renkDegis(item:UITabBarItemAppearance) {
+        //Seçili Olan Durum
+        item.selected.iconColor = UIColor.systemOrange
+        item.selected.titleTextAttributes = [.foregroundColor: UIColor.systemOrange]
+        item.selected.badgeBackgroundColor = .systemMint
+        
+        //Seçili Olmayan Durum
+        item.normal.iconColor = UIColor.white
+        item.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        item.normal.badgeBackgroundColor = .lightGray
     }
 
 
