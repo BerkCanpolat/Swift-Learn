f634546af6ea779a7f22e2ca0bb7b961c85aabd6
Berk Canpolat <canpolatberk19@gmail.com>
2023-12-20 21:47:49 +0300

    filmlerApp sqlite added

--- IOS-BootCamp/8.Gun/FilmlerApp/FilmlerApp/data/repo/FilmlerDoaRepository.swift
+++ IOS-BootCamp/8.Gun/FilmlerApp/FilmlerApp/data/repo/FilmlerDoaRepository.swift
@@ -11,21 +11,40 @@ import RxSwift
 class FilmlerDoaRepository {
     var filmlerListe = BehaviorSubject<[Filmler]>(value: [Filmler]())
     
+    let db: FMDatabase?
+    
+    init() {
+        let hedefYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let veritabaniUrl = URL(string: hedefYolu)!.appendingPathComponent("filmler_app.sqlite")
+        db = FMDatabase(path: veritabaniUrl.path)
+    }
+    
     func filmleriYukle() {
-        var liste = [Filmler]()
-        let f1 = Filmler(id: 1, ad: "Django", resim: "django", fiyat: 24)
-        let f2 = Filmler(id: 2, ad: "Interstellar", resim: "interstellar", fiyat: 32)
-        let f3 = Filmler(id: 3, ad: "Inception", resim: "inception", fiyat: 16)
-        let f4 = Filmler(id: 4, ad: "The Hateful Eight", resim: "thehatefuleight", fiyat: 28)
-        let f5 = Filmler(id: 5, ad: "The Pianist", resim: "thepianist", fiyat: 18)
-        let f6 = Filmler(id: 6, ad: "Anadoluda", resim: "anadoluda", fiyat: 10)
         
-        liste.append(f1)
-        liste.append(f2)
-        liste.append(f3)
-        liste.append(f4)
-        liste.append(f5)
-        liste.append(f6)
-        filmlerListe.onNext(liste)
+
+        
+        
+        db?.open()
+        
+        var liste = [Filmler]()
+        
+        do {
+            let rs = try db!.executeQuery("SELECT * FROM filmler", values: nil)
+            
+            while rs.next() {
+                let film = Filmler(id: Int(rs.string(forColumn: "id"))!,
+                                   ad: rs.string(forColumn: "ad")!,
+                                   resim: rs.string(forColumn: "resim")!,
+                                   fiyat: Int(rs.string(forColumn: "fiyat"))!)
+                
+                liste.append(film)
+            }
+            filmlerListe.onNext(liste)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        db?.close()
     }
 }
--- IOS-BootCamp/8.Gun/FilmlerApp/FilmlerApp/ui/viewmodel/AnasayfaViewModel.swift
+++ IOS-BootCamp/8.Gun/FilmlerApp/FilmlerApp/ui/viewmodel/AnasayfaViewModel.swift
@@ -13,11 +13,29 @@ class AnasayfaViewModel {
     var filmlerListe = BehaviorSubject<[Filmler]>(value: [Filmler]())
     
     init() {
+        veritabaniKopyala()
         filmlerListe = frepo.filmlerListe
         filmleriYukle()
     }
     
     func filmleriYukle() {
         frepo.filmleriYukle()
+    }
+    
+    
+    func veritabaniKopyala() {
+        let bundleYolu = Bundle.main.path(forResource: "filmler_app", ofType: ".sqlite")
+        let hedefYolu = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let kopyalanacakYer = URL(string: hedefYolu)!.appendingPathComponent("filmler_app.sqlite")
+        let fileManager = FileManager.default
+        if fileManager.fileExists(atPath: kopyalanacakYer.path) {
+            print("Veri tabanı zaten var")
+        } else {
+            do {
+                try fileManager.copyItem(atPath: bundleYolu!, toPath: kopyalanacakYer.path)
+            } catch {
+                
+            }
+        }
     }
 }
