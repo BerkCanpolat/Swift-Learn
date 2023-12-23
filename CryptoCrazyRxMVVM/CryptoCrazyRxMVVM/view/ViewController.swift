64bf478b660646283a8065e6fa3d88838bdda9db
Berk Canpolat <canpolatberk19@gmail.com>
2023-12-23 14:09:41 +0300

    Crypto Crazy App MVVM and RxSwift added

--- CryptoCrazyRxMVVM/CryptoCrazyRxMVVM/view/ViewController.swift
+++ CryptoCrazyRxMVVM/CryptoCrazyRxMVVM/view/ViewController.swift
@@ -11,22 +11,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var tableView: UITableView!
     
+    var cryptoList = [Crypto]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return cryptoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = "Crypto Currency"
-        content.secondaryText = "Crypto Price"
+        content.text = cryptoList[indexPath.row].currency
+        content.secondaryText = cryptoList[indexPath.row].price
         cell.contentConfiguration = content
         return cell
     }
