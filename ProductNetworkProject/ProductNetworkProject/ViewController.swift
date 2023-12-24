5c3f01928076b94e1c0e16b062c3f9a85eb44393
Berk Canpolat <canpolatberk19@gmail.com>
2023-12-24 20:26:51 +0300

    Network Learn SearchBar Added

--- ProductNetworkProject/ProductNetworkProject/ViewController.swift
+++ ProductNetworkProject/ProductNetworkProject/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    let searchController = UISearchController()
+    
     var model: [Model] = []
     
     override func viewDidLoad() {
@@ -19,10 +21,13 @@ class ViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        getData()
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        
+        getData(arananKelime: "")
     }
     
-    func getData() {
+    func getData(arananKelime: String) {
         var url = "https://dummyjson.com/products"
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = "GET"
@@ -37,15 +42,28 @@ class ViewController: UIViewController {
                     if let json = try! JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any] {
                         
                         if let products = json["products"] as? [[String:Any]] {
-                            print(products)
                             var imagesArray = [Images]()
+                            self.model.removeAll()
                             for product in products {
-                                self.model.append(Model(
-                                    title: "\(product["title"]!)",
-                                    description: "\(product["description"]!)",
-                                    price: product["price"] as? Int,
-                                    thumbnail: "\(product["thumbnail"]!)",
-                                    images: imagesArray))
+                                
+                                //! işareti varsa arananKelime boş değilse demek. ! yoksa aranan kelime boşsa demek.
+                                if !arananKelime.isEmpty {
+                                    
+                                    if (product["title"] as? String)?.lowercased().range(of: arananKelime.lowercased()) != nil {
+                                        self.model.append(Model(
+                                            title: "\(product["title"]!)",
+                                            thumbnail: "\(product["thumbnail"]!)"))
+                                    }
+                                    
+                                } else {
+                                    self.model.append(Model(
+                                        title: "\(product["title"]!)",
+                                        description: "\(product["description"]!)",
+                                        price: product["price"] as? Int,
+                                        thumbnail: "\(product["thumbnail"]!)",
+                                        images: imagesArray))
+                                }
+
                             }
                             
                             DispatchQueue.main.async {
@@ -66,7 +84,16 @@ class ViewController: UIViewController {
 
 
 
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        
+        getData(arananKelime: text)
+    }
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return model.count
@@ -78,9 +105,13 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCell
         
-        cell.productLabel.text = m.title
-        cell.imageView.sd_setImage(with: URL(string: m.thumbnail!))
-        
+        if indexPath.row < model.count {
+            cell.productLabel.text = m.title
+            cell.imageView.sd_setImage(with: URL(string: m.thumbnail!))
+        } else {
+            cell.productLabel.text = m.title
+            cell.imageView.sd_setImage(with: URL(string: m.thumbnail!))
+        }
         
         return cell
     }
