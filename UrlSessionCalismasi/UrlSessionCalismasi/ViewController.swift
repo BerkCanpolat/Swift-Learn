48fe7fc83b69a8f65a2d0f797f4e156aa5ac867e
Berk Canpolat <canpolatberk19@gmail.com>
2024-01-03 20:03:28 +0300

    udemy network finish

--- UrlSessionCalismasi/UrlSessionCalismasi/ViewController.swift
+++ UrlSessionCalismasi/UrlSessionCalismasi/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        kisiEkle()
+        //kisiEkle()
+        //kisiSil()
+        //kisiGuncelle()
+        tumKisiler()
+        //aramaYap()
     }
 
     func kisiEkle() {
@@ -26,6 +30,107 @@ class ViewController: UIViewController {
         URLSession.shared.dataTask(with: request) { data, response, error in
             if error != nil || data == nil {
                 print("error")
+                return
+            } else {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any] {
+                        print(json)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+    }
+    
+    func kisiSil() {
+        
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/delete_kisiler.php")!)
+        request.httpMethod = "POST"
+        let postString = "kisi_id=16568"
+        request.httpBody = postString.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error != nil || data == nil {
+                print("delete error")
+                return
+            } else {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any] {
+                        print(json)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+        
+    }
+    
+    func kisiGuncelle() {
+        
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/update_kisiler.php")!)
+        request.httpMethod = "POST"
+        let postString = "kisi_id=16567&kisi_ad=Deistirdimbro&kisi_tel=telideistirdim"
+        request.httpBody = postString.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error != nil || data == nil {
+                print("delete error")
+                return
+            } else {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any] {
+                        print(json)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+        
+    }
+    
+    func tumKisiler() {
+        let url = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/tum_kisiler.php")!)
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if error != nil || data == nil {
+                print("kisiler gelmedi")
+                return
+            } else {
+                do {
+                    let json = try JSONDecoder().decode(KisiCevap.self, from: data!)
+                    
+                    print(json.kisiler![1].kisi_ad!)
+                        
+                        
+                        /*if let result = json["kisiler"] as? [[String:Any]] {
+                            for i in result {
+                                print("kisi id: \(i["kisi_id"]!)")
+                                print("kisi ad: \(i["kisi_ad"]!)")
+                                print("kisi tel: \(i["kisi_tel"]!)")
+                            }
+                        }*/
+                        
+                        //print(json)
+                    
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+    }
+    
+    func aramaYap() {
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/tum_kisiler_arama.php")!)
+        request.httpMethod = "POST"
+        let postString = "kisi_ad=m"
+        request.httpBody = postString.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error != nil || data == nil {
+                print("kisiler gelmedi")
                 return
             } else {
                 do {
