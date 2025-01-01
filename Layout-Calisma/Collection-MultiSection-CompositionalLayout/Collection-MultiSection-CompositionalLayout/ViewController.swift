9f0991259ab6e4b45b71b9784628a309a362fb5c
Berk Canpolat <canpolatberk19@gmail.com>
2025-01-01 04:52:24 +0300

    Multiple Section CollectionView Compositional Layout project start

---
+++ Layout-Calisma/Collection-MultiSection-CompositionalLayout/Collection-MultiSection-CompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection-MultiSection-CompositionalLayout
+//
+//  Created by Berk Canpolat on 1.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
