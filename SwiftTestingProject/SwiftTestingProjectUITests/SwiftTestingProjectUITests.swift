5ec9a5aac1c793df6a154896d1cce90670cbf752
Berk Canpolat <canpolatberk19@gmail.com>
2025-01-23 12:38:06 +0300

    Todolist test finish

--- SwiftTestingProject/SwiftTestingProjectUITests/SwiftTestingProjectUITests.swift
+++ SwiftTestingProject/SwiftTestingProjectUITests/SwiftTestingProjectUITests.swift
@@ -9,7 +9,6 @@ import XCTest
 
 final class SwiftTestingProjectUITests: XCTestCase {
 
-    
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -19,22 +18,49 @@ final class SwiftTestingProjectUITests: XCTestCase {
         
         let elementsQuery = app.alerts["Add Item"].scrollViews.otherElements
         let textField = elementsQuery.collectionViews/*@START_MENU_TOKEN@*/.textFields["Enter Item"]/*[[".cells.textFields[\"Enter Item\"]",".textFields[\"Enter Item\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let addedCell = app.tables.cells.containing(.staticText, identifier: "berk").element
         
         let okButton = elementsQuery.buttons["OK"]
-                
         
         addButton.tap()
         
         textField.tap()
-        textField.typeText("Berk")
+        textField.typeText("berk")
         okButton.tap()
         
-        // Eklenen hücrenin varlığını bekleme
-            let newCell = app.tables.cells.element(boundBy: 0)
-            XCTAssertTrue(newCell.waitForExistence(timeout: 5), "Eklenen hücre bulunamadı.")
-            
-            // Hücreye tıklama
-            newCell.tap()
+        XCTAssertTrue(addedCell.exists)
+    }
+    
+    func testDelete() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        let addButton = app.navigationBars["SwiftTestingProject.View"].buttons["Item"]
+        let elementsQuery = app.alerts["Add Item"].scrollViews.otherElements
+        let textField = elementsQuery.collectionViews.textFields["Enter Item"]
+        //textField.tap()
+        //textField.typeText("berk")
+                
+        let okButton = elementsQuery.buttons["OK"]
+        
+        let addedCell = app.tables.cells.containing(.staticText, identifier: "berk").element
+        
+        
+
+        let tablesQuery = app.tables
+        let deleteButton = tablesQuery.buttons["Delete"]
+        
+        addButton.tap()
+        
+        textField.tap()
+        textField.typeText("berk")
+        okButton.tap()
+        
+        addedCell.swipeLeft()
+        deleteButton.tap()
+        
+        XCTAssertFalse(addedCell.exists)
+        
     }
     
 }
