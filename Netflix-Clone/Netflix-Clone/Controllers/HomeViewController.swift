b145973cafa30a817c81c6d3989a2e45519cd9bf
Berk Canpolat <canpolatberk19@gmail.com>
2025-02-06 22:13:04 +0300

    Netflix clone started

---
+++ Netflix-Clone/Netflix-Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Netflix-Clone
+//
+//  Created by Berk Canpolat on 6.02.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
