4595bf812a0db2440c0a81531c495af4a3ac6358
Berk Canpolat <canpolatberk19@gmail.com>
2025-02-19 23:04:07 +0300

    Finish programatic searchui

--- ProgrammaticUILearn/ProgrammaticUILearn/SearchProgramatic/SearchViewController.swift
+++ ProgrammaticUILearn/ProgrammaticUILearn/SearchProgramatic/SearchViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    let searchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,29 +20,66 @@ class SearchViewController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         
+        searchBar.delegate = self
+        
+        searchBar.sizeToFit()
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Search Bar"
         
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(handleShowSearchBar))
-        
         let appereance = UINavigationBarAppearance()
         appereance.configureWithOpaqueBackground()
-        appereance.backgroundColor = UIColor(red: 55/255, green: 120/255, blue: 250/255, alpha: 1)
+        //appereance.backgroundColor = UIColor(red: 55/255, green: 120/255, blue: 250/255, alpha: 1)
         appereance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        appereance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appereance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
         
         navigationController?.navigationBar.barStyle = .black
-        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.tintColor = .black
         navigationController?.navigationBar.isTranslucent = false
         
         navigationController?.navigationBar.standardAppearance = appereance
         navigationController?.navigationBar.scrollEdgeAppearance = appereance
         navigationController?.navigationBar.compactAppearance = appereance
+        
+        showSearchButton(shouldShow: true)
     }
     
     @objc func handleShowSearchBar() {
-        print("tikladim")
+        search(shouldShow: true)
+        searchBar.becomeFirstResponder()
+    }
+    
+    func search(shouldShow: Bool) {
+        showSearchButton(shouldShow: !shouldShow)
+        searchBar.showsCancelButton = shouldShow
+        navigationItem.titleView = shouldShow ? searchBar : nil
+        
+    }
+    
+    func showSearchButton(shouldShow: Bool) {
+        if shouldShow {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(handleShowSearchBar))
+        } else {
+            navigationItem.rightBarButtonItem = nil
+        }
     }
 
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        search(shouldShow: false)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("Search: \(searchText)")
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("Searchbar end editing")
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("Searchbar begin editing")
+    }
 }
