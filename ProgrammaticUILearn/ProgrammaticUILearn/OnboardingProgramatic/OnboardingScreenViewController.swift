b86ff8294a061ee9354f3c97938ece8651bbdab2
Berk Canpolat <canpolatberk19@gmail.com>
2025-02-24 23:39:16 +0300

    Onboarding screen programatic ui finish

--- ProgrammaticUILearn/ProgrammaticUILearn/OnboardingProgramatic/OnboardingScreenViewController.swift
+++ ProgrammaticUILearn/ProgrammaticUILearn/OnboardingProgramatic/OnboardingScreenViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class OnboardingScreenViewController: UIViewController {
     
+    //MARK: - UIElements
     private lazy var bearImageView: UIImageView = {
         var img = UIImage(named: "1")
         let imageView = UIImageView(image: img)
@@ -38,42 +39,82 @@ class OnboardingScreenViewController: UIViewController {
         text.isScrollEnabled = false
         return text
     }()
+    
+    private lazy var previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Prev", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    private lazy var nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.systemPink, for: .normal)
+        return button
+    }()
+    
+    private lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .red
+        pc.pageIndicatorTintColor = .gray
+        return pc
+    }()
 
 
 
+
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        //view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
-        //imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
 
-        
+        setupBottomControls()
         setupLayout()
+    }
+    
+    
+    //MARK: -FUNCTIONS
+    private func setupBottomControls() {
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton,pageControl,nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     private func setupLayout() {
         let topImageContainerView = UIView()
-        //topImageContainerView.backgroundColor = .blue
         view.addSubview(topImageContainerView)
-        //topImageContainerView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         
         topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
         topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
         topImageContainerView.addSubview(bearImageView)
         bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
         bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
         bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
-        //bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        //bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        //bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
-        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 120).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 5).isActive = true
         descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
         descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
---
+++ ProgrammaticUILearn/ProgrammaticUILearn/OnboardingProgramatic/SwippingController.swift
@@ -0,0 +1,15 @@
+//
+//  SwippingController.swift
+//  ProgrammaticUILearn
+//
+//  Created by Berk Canpolat on 24.02.2025.
+//
+
+import UIKit
+
+class SwippingController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        collectionView.backgroundColor = .green
+    }
+}
