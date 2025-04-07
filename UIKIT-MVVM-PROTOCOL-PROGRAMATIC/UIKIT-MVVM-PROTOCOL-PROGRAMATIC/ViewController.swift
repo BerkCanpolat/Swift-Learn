a164ea8241ee6af436fc85dabb2c18bda0e08da6
Berk Canpolat <canpolatberk19@gmail.com>
2025-04-07 18:47:58 +0300

    uikit mvvm-protocol started project

---
+++ UIKIT-MVVM-PROTOCOL-PROGRAMATIC/UIKIT-MVVM-PROTOCOL-PROGRAMATIC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKIT-MVVM-PROTOCOL-PROGRAMATIC
+//
+//  Created by Berk Canpolat on 7.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
